e5e7361ef57519556f548bf726fdc2f6a730eedf
Michelle Bell <michbesch0@outlook.com>
2024-04-04 14:20:57 -0400

    Created the handleDrop function

--- assets/js/script.js
+++ assets/js/script.js
@@ -122,7 +122,18 @@ function handleDeleteTask(event){
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
+    const tasks = JSON.parse(localStorage.getItem("tasks"));
+    const taskIdDrop = ui.draggable[0].dataset.taskId;
+    const newStatus = event.target.id;
 
+    for (let task of tasks) {
+        if (task.id === taskIdDrop) {
+            task.status = newStatus;
+        }
+    }
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    renderTaskList();
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
@@ -131,7 +142,7 @@ $(document).ready(function () {
 });
 
 // When the "Add Task" button is clicked, a modal pops up with a form to input task content 
-const formModal = document.getElementById('formModal')
+const formModal = document.getElementById('formModal');
 if (formModal) {
     formModal.addEventListener('show.bs.modal', event => {
     const button = event.relatedTarget;
